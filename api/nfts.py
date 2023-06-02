80071955f43e20b4a5b6c5693ec906c04342d812
BobTheFarmer <colinmweis@gmail.com>
2023-06-02 15:25:49 -0700

    Undo bad fix

--- api/nfts.py
+++ api/nfts.py
@@ -78,7 +78,7 @@ class NftAPI(Resource):
             if nft is None:
                 return {'message': f'nft not found'}, 404
 
-            profile = body.get('profile')
+            profile = body.get('userID')
             if profile is not None:
                 nft.update(profile)
             
